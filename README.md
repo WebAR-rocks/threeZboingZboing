c7fe1757ac7e8b035e40b5193b77b68db7f8aaca
Xavier Bourry <xavier.bourry@polytechnique.org>
2021-03-08 00:47:31 -0500

    [INIT]

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # threeZboingZboing
+
+
+Simple bone based physics for THREE.js.
+Simulates a spring damper system applied at the rotation of bone joints.
+Once physics is applied, the instance of `THREE.SkinnedMesh` can be moved, rotated, animated using a skeleton and physics will automatically apply.
+
+
+## Architecture
+
+* `index.html`: demo HTML file, to host with a static HTTP server,
+* `assets/poulpi.blend`: Blender octopus rigged 3D model,
+* `assets/poulpi.glb`: Octopus rigged 3D model exported using Blender GLTF exporter,
+* `src/main.js`: main demo script,
+* `src/ZboingZboingPhysics.js`: the most important file of this library. You only need this one
+
+
+## Usage
+
+Import the module:
+
+```javascript
+import { ZboingZboingPhysics } from './ZboingZboingPhysics.js';
+```
+
+Create physics:
+
+```javascript
+const physics = new ZboingZboingPhysics(scene, skinnedMesh, bonePhysics, options);
+```
+
+Where:
+
+* `<THREE.Scene> scene` is the Three scene. Bones simulated using physics will be directly put there,
+* `<THREE.SkinnedMesh> skinnedMesh` is the skinned mesh to apply physics,
+* `<Dict> bonePhysics` is a dictionnary with bones physical parameters:
+
+   ```
+   DEFAULT: {
+      damperRange: [0.001, 0.005],
+      springRange: [0.000001, 0.000005]
+    },
+    boneName: {
+      damper: 0.0008,
+      spring: 0.000004
+    },
+    anotherBoneName: {
+      damper: 0.0008,
+      spring: 0.000004
+    }
+    ```
+* `<Dict> options` are some useless options
+
+
+**WARNING**: to animate your skinned mesh, you should not directly create an instance of `THREE.AnimationMixer` using the skinned mesh. You need to proceed this way:
+
+```javascript
+const animationMixer = physics.create_animationMixer();
+```
+
+Update (in the rendering loop):
+```javascript
+physics.update();
+```
+
+Destroy:
+```javascript
+physics.destroy();
+````
+
+## How it works
+
+When we create the physics, we replace the swap the skeleton of the skinned mesh by a new skeleton where bone will be simulated using physics.
+The old skeleton is still here and called the rigid skeleton. But it is hidden. Each bone of the new skeleton is simulated using damper-spring systems:
+
+* between the start of the bone and the start of the rigid skeleton bone
+* between the start of the bone and its parent
+* between the end of the bone and its children
+
+
+## License
+
+[MIT software license](LICENSE)
+
+Files included in these directories are NOT affected by this license:
+
+* `/libs/`: each libs is released under its own software license
+* `/assets/`: The octopus 3D model is released under CC license
+
+
+## References
+
+3D Model: [Blue Ringed Octopus by Leslie Stowe](https://sketchfab.com/3d-models/blue-ringed-octopus-inktober-day-1-ac85becda709471494d049ab346136f0)
---
+++ src/main.js
@@ -0,0 +1,146 @@
+import * as THREE from '../libs/three/v126/build/three.module.js';
+
+import { OrbitControls } from '../libs/three/v126/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../libs/three/v126/examples/jsm/loaders/GLTFLoader.js';
+
+import { ZboingZboingPhysics } from './ZboingZboingPhysics.js';
+
+
+
+
+window.PARENTTHREEOBJ = null;
+
+
+const SETTINGS = {
+  meshPath: 'assets/poulpi.glb',
+  skinnedMeshName: null,
+  
+  bonesPhysics: {
+    DEFAULT: {
+      damperRange: [0.001, 0.005],
+      springRange: [0.00001, 0.000005]
+    } /*
+    boneName: {
+      damper: 0.0008,
+      spring: 0.000004
+    }//*/
+  }
+};
+
+
+
+let renderer = null, camera = null, scene = null, controls = null;
+
+let skinnedMesh = null, animationMixer = null, clock = null, animationAction = null;
+let physics = null;
+
+// entry point:
+function main(){
+  const canvas = document.getElementById('three');
+
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+    canvas: canvas
+  });
+
+  scene = new THREE.Scene();
+  
+  camera = new THREE.PerspectiveCamera(30, 1, 0.1, 1000 );
+  camera.aspect = canvas.width / canvas.height;
+  camera.position.set(0, 0, 5);
+  camera.updateProjectionMatrix();
+
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.update();
+
+  // lighting:
+  const al = new THREE.AmbientLight(0xffffff, 1);
+  const pl = new THREE.PointLight(0xffffff, 2);
+  pl.position.set(0,0,10);
+  scene.add(al, pl);
+
+  // load mesh:
+  const loader = new GLTFLoader();
+  loader.load( SETTINGS.meshPath, function ( gltf ) {
+    
+    window.gltf = gltf;
+    
+    const model = gltf.scene;
+    model.position.set(0,-0.5,0);
+    model.scale.multiplyScalar(0.5);
+    scene.add(model);
+
+    skinnedMesh = extract_skinnedMesh(model, SETTINGS.skinnedMeshName);
+
+    // put some stuffs as globals:
+    window.skinnedMesh = skinnedMesh;
+    window.PARENTTHREEOBJ = model;
+    
+    physics = new ZboingZboingPhysics(scene, skinnedMesh, SETTINGS.bonesPhysics, {
+      isDebug: false
+    });
+
+    // handle animation:
+    if (gltf.animations.length > 0){
+      const animationClip = gltf.animations[0];
+
+      // we need to use physics to create the animationMixer
+      // because it will be linked to the rigid (and hidden) skeleton
+      animationMixer = physics.create_animationMixer();
+      clock = new THREE.Clock();
+      animationAction = animationMixer.clipAction( animationClip );
+      animationAction.play();
+      window.animationAction = animationAction;
+    }
+
+    console.log('Start animate...');
+    animate();
+  });
+}
+
+
+function extract_skinnedMesh(model, name){
+  let sm = null;
+  model.traverse(function(threeStuff){
+    if (threeStuff.type === "SkinnedMesh" 
+      && (!SETTINGS.skinnedMeshName || threeStuff.name === SETTINGS.skinnedMeshName)){
+      sm = threeStuff;
+    }
+  });
+  return sm;
+}
+
+
+function animate(dt){
+  controls.update();
+
+  if (physics !== null){
+    physics.update();
+    if (animationMixer){    
+      animationMixer.update(clock.getDelta());
+    }
+  }
+
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+}
+
+
+function toggle_animation(){
+  if (!animationAction) {
+    console.log('NO ANIMATION');
+    return;
+  }
+  if (animationAction.paused){
+    console.log('PLAY ANIMATION');
+    animationAction.paused = false;
+  } else {
+    console.log('PAUSE ANIMATION');
+    animationAction.paused = true;
+  }
+}
+
+
+window.toggle_animation = toggle_animation;
+window.onload = main;
